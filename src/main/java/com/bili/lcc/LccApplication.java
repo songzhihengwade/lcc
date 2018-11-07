62e1659ec619c8bdeade2e5c53d0f64d33c6b2dc
songzhiheng <18079932230@163.com>
2018-11-07 19:48:49 +0800

    first

---
+++ src/main/java/com/bili/lcc/LccApplication.java
@@ -0,0 +1,12 @@
+package com.bili.lcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LccApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bili/lcc/LccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bili.lcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LccApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
